97d515e218a249fe74c7d5175ad010750d6ac130
John Greb <karikoa@One.greyskull>
2014-07-26 10:00:06 +0100

    Increase WebP subimages to 64x64.

--- webp-test.sh
+++ webp-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for y in {0,1,2,3,4}; do
-for x in {0,1,2,3,4,5}; do
-cwebp -short -segments 2 -size 230 -pass 5 -crop $((48*x)) $((48*y)) 48 48 test.jpg -o test.webp
+# image size 320x240 as 5x4 64x64 pixel blocks
+for y in {0,1,2,3}; do
+for x in {0,1,2,3,4}; do
+cwebp -short -segments 1 -size 230 -pass 9 -crop $((64*x)) $((64*y)) 64 64 test.jpg -o test.webp
 ssdv -i 0 -p $((16*$y+$x)) -c TEST test.webp test.ssdv
 ssdv -d test.ssdv newtest.webp
 dwebp -ppm newtest.webp -o test$x.ppm
