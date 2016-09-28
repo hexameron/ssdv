5adec26588a5168c3883721f5c8dd6eee8d1095c
hexameron <hexameron>
2016-09-28 15:38:20 +0100

    320x16 blocksize.

--- webp-test.sh
+++ webp-test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# image size 320x240 as 5x4 64x64 pixel blocks
-for y in {0,1,2,3}; do
-for x in {0,1,2,3,4}; do
-cwebp -short -segments 1 -size 230 -pass 9 -crop $((64*x)) $((64*y)) 64 64 test.jpg -o test.webp
-ssdv -i 0 -p $((16*$y+$x)) -c TEST test.webp test.ssdv
+# image size 320x128 as 8 320x16 pixel blocks
+for y in {0,1,2,3,4,5,6,7}; do
+cwebp -short -segments 1 -size 230 -pass 9 -crop 0 $((16*y)) 320 16 test.jpg -o test.webp
+ssdv -i 0 -p $((y)) -c TEST test.webp test.ssdv
 ssdv -d test.ssdv newtest.webp
-dwebp -ppm newtest.webp -o test$x.ppm
+dwebp -ppm newtest.webp -o test$y.ppm
 done
-convert +append test*ppm testout$y.jpg
-done
-convert -append testout*jpg result.jpg
+convert -append test*ppm testout.jpg
